cba214ad83fcda3543600d4d3b87dbd4edc83f6a
bradleyhey <BradleyHey@users.noreply.github.com>
2026-02-17 23:32:09 -0600

    Add Movie app

---
+++ MovieApp/MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,78 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.Models;
+
+namespace MovieApp.Controllers;
+public class MovieController : Controller
+{
+    private MovieContext context { get; set; }
+
+    public MovieController(MovieContext ctx)
+    {
+        context = ctx;
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        ViewBag.Action = "Add";
+        ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList(); 
+        return View("Edit", new Movie());
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        ViewBag.Action = "Edit";
+        ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList(); 
+        var movie = context.Movies.Find(id);
+        return View(movie);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(Movie movie) 
+    { 
+        if (ModelState.IsValid) {
+            if (movie.MovieId == 0)
+                context.Movies.Add(movie);
+            else
+                context.Movies.Update(movie); 
+            context.SaveChanges();
+            return RedirectToAction("Index", "Movie");
+        } else {
+            ViewBag.Action = (movie.MovieId == 0) ? "Add": "Edit"; 
+            ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList(); 
+            return View(movie); 
+        } 
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var movie = context.Movies.Find(id);
+        return View(movie);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Movie movie)
+    {
+        context.Movies.Remove(movie);
+        context.SaveChanges();
+        return RedirectToAction("Index", "Movie");
+    }
+    
+    // public IActionResult Index()
+    // {
+    //     var movies = context.Movies
+    //         .OrderBy(m => m.Name)
+    //         .ToList();
+    //
+    //     return View(movies);
+    // }
+
+    public IActionResult Index()
+    {
+        var movies = context.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+        return View(movies);
+    }
+}
---
+++ MovieApp/MovieApp/Models/Genre.cs
@@ -0,0 +1,7 @@
+namespace MovieApp.Models;
+
+public class Genre
+{
+    public string GenreId { get; set; } 
+    public string Name { get; set; }
+}
