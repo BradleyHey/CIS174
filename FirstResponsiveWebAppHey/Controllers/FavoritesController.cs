278f6d160cf63670d84c9991f6798f9bdcfff7fa
bradleyhey <BradleyHey@users.noreply.github.com>
2026-04-25 21:01:33 -0500

    Added favorites assignment functionality

---
+++ FirstResponsiveWebAppHey/Controllers/FavoritesController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FirstResponsiveWebAppHey.Models.Olympics;
+using System.Linq;
+
+namespace FirstResponsiveWebAppHey.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private OlympicsContext context;
+        public FavoritesController(OlympicsContext ctx) => context = ctx;
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var session = new OlympicsSession(HttpContext.Session);
+            var model = new OlympicsViewModel
+            {
+                ActiveGame = session.GetActiveGame(),
+                ActiveCat = session.GetActiveCat(),
+                Countries = session.GetMyCountries()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Add(Country country)
+        {
+            country = context.Countries
+                 .Include(c => c.Game)
+                 .Include(c => c.Category)
+                 .Where(c => c.CountryID == country.CountryID)
+                 .FirstOrDefault() ?? new Country();
+
+            var session = new OlympicsSession(HttpContext.Session);
+            var countries = session.GetMyCountries();
+            countries.Add(country);
+            session.SetMyCountries(countries);
+
+            TempData["message"] = $"{country.Name} added to your favorites";
+
+            return RedirectToAction("Index", "Olympics", 
+                new
+                {
+                    ActiveGame = session.GetActiveGame(),
+                    ActiveCat = session.GetActiveCat()
+                });
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete()
+        {
+            var session = new OlympicsSession(HttpContext.Session);
+            session.RemoveMyCountries();
+
+            TempData["message"] = "Favorite countries cleared";
+
+            return RedirectToAction("Index", "Olympics",
+                new {
+                    ActiveGame = session.GetActiveGame(),
+                    ActiveCat = session.GetActiveCat()
+                });
+        }
+    }
+}
--- FirstResponsiveWebAppHey/Models/Olympics/OlympicsSession.cs
+++ FirstResponsiveWebAppHey/Models/Olympics/OlympicsSession.cs
@@ -1,14 +1,28 @@
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 
 namespace FirstResponsiveWebAppHey.Models.Olympics
 {
     public class OlympicsSession
     {
+        private const string CountriesKey = "mycountries";
+        private const string CountKey = "countrycount";
         private const string GameKey = "game";
         private const string CatKey = "cat";
 
         private ISession session { get; set; }
         public OlympicsSession(ISession session) => this.session = session;
+
+        public void SetMyCountries(List<Country> countries)
+        {
+            session.SetObject(CountriesKey, countries);
+            session.SetInt32(CountKey, countries.Count);
+        }
+
+        public List<Country> GetMyCountries() =>
+            session.GetObject<List<Country>>(CountriesKey) ?? new List<Country>();
+
+        public int? GetMyCountryCount() => session.GetInt32(CountKey);
 
         public void SetActiveGame(string activeGame) =>
             session.SetString(GameKey, activeGame);
@@ -19,5 +33,11 @@ namespace FirstResponsiveWebAppHey.Models.Olympics
             session.SetString(CatKey, activeCat);
         public string GetActiveCat() => 
             session.GetString(CatKey) ?? "all";
+
+        public void RemoveMyCountries()
+        {
+            session.Remove(CountriesKey);
+            session.Remove(CountKey);
+        }
     }
 }
